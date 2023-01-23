240d3ac86a6db3ee45d927532e695c5b82d20950
Mate Radnai <mate_radnai@epam.com>
2023-01-23 19:44:32 +0100

    update test with baseurl

--- helpers/get-requests.js
+++ helpers/get-requests.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
 const axios = require('axios');
 
+const baseUrl = process.env.BASE_URL;
+
 const getUsers = async () => {
-    const response = await axios.get('https://reqres.in/api/users');
+    const response = await axios.get(`${baseUrl}/users`);
     // console.log("RESPONSE: ", response);
     return response;
 };
