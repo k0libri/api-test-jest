565d71432d5b9fbcb995bd2977a568a86781d3c1
Mate Radnai <mate_radnai@epam.com>
2023-07-12 23:26:56 +0200

    add reporting

---
+++ jest-stare.config.ts
@@ -0,0 +1,8 @@
+module.exports = {
+    resultDir: './jest-stare',
+    reportTitle: 'Jest Test Report',
+    reportHeadline: 'Jest Test Report',
+    coverageLink: '../coverage/lcov-report/index.html',
+    reportDir: '../jest-stare-report',
+    additionalResultsProcessors: [],
+};
